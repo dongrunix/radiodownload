a7523742ec271e1b01a1749909a43501c3194c71
dongrunix <dongrunix@gmail.com>
2018-09-07 17:08:22 +0800

    Update radiodownload.py

--- radiodownload.py
+++ radiodownload.py
@@ -1,6 +1,5 @@
 #coding:utf-8 
 import os
-from bs4 import BeautifulSoup
 from selenium import webdriver
 import wget
 
@@ -8,18 +7,11 @@ def download(url):
     #Chrome改FireFox
     driver=webdriver.Chrome()
     driver.get(url)
-    html=driver.page_source
+    allherf=driver.find_elements_by_link_text('下载')
+    for i in range(len(allherf)):
+        print(allherf[i].get_attribute('onclick').split("\'")[1].split('/')[-1])
+        wget.download('http://'+allherf[i].get_attribute('onclick').split("\'")[1],allherf[i].get_attribute('onclick').split("\'")[1].split('/')[-1])     
     driver.close()
-    soup = BeautifulSoup(html,'lxml')
-    allherf=soup.find_all('a')
-    for herf in allherf:
-        try:
-            if('.m4a' in herf.attrs['onclick']):
-                print(herf.attrs['onclick'].split("\'")[1].split('/')[-1])
-                wget.download('http://'+herf.attrs['onclick'].split("\'")[1],herf.attrs['onclick'].split("\'")[1].split('/')[-1])     
-        except:
-            pass
-
 if __name__=='__main__':
     url='http://www.radio.cn/pc-portal/sanji/zhibo_2.html?channelname=2&name=520799&title=radio'
     download(url)
